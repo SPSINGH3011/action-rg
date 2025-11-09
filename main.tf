96f72a39f069aaa353cf97d4d9bcad60d6254190
Satya Prakash <spsingh302212@gmail.com>
2025-11-09 18:58:14 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.52.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  feature {}
+  subscription_id = "e7e9c8a1-3a57-431d-a76d-9cf0dcda292b"
+}
+
+
+resource "azurerm_resource_group" "action" {
+  name     = "action-rg"
+  location = "West Europe"
+}
